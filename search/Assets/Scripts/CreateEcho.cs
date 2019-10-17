b83a7f98c6abaa503d5daf8cf3195390619bd182
大島有貴 <yuki.nasu.0225@docomo.ne.jp>
2019-10-18 00:23:58 +0900

    unity_Data

---
+++ search/Assets/Scripts/CreateEcho.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEcho : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+		transform.localPosition=Vector3.Lerp(transform.localPosition,new Vector3(3,0,0),Time.deltaTime * 1);
+		transform.localScale = Vector3.Lerp(transform.localScale,new Vector3(3,3,3),Time.deltaTime * 3);
+    }
+		
+}
