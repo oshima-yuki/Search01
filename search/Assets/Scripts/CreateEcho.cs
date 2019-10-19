09cd1519527a35636282df07fdebd205f48a0aa3
大島有貴 <yuki.nasu.0225@docomo.ne.jp>
2019-10-20 03:45:10 +0900

    Echo object

--- search/Assets/Scripts/CreateEcho.cs
+++ search/Assets/Scripts/CreateEcho.cs
@@ -9,6 +9,5 @@ public class CreateEcho : MonoBehaviour
     {
 		transform.localPosition=Vector3.Lerp(transform.localPosition,new Vector3(3,0,0),Time.deltaTime * 1);
 		transform.localScale = Vector3.Lerp(transform.localScale,new Vector3(3,3,3),Time.deltaTime * 3);
-    }
-		
+    }		
 }
